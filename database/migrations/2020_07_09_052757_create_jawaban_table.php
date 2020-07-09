b35ff85765d863d0f6b14dc9c317738bbbab63bd
syauqisabili <syauqisabili@gmail.com>
2020-07-09 14:57:44 +0700

    Menambahh migrations tetapi belum di-migrate

---
+++ database/migrations/2020_07_09_052757_create_jawaban_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateJawabansTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('jawaban', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('isi');
+            $table->boolean('status')->nullable();
+            $table->timestamps();
+
+            $table->unsignedBigInteger('user_id');
+
+            //foreign key ke tabel users
+            $table  ->foreign('user_id')
+                    ->references('id')
+                    ->on('users')
+                    ->onDelete('cascade')
+                    ->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('jawaban');
+    }
+}
---
+++ database/migrations/2020_07_09_073157_create_vote_table.php
@@ -0,0 +1,58 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVoteTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vote', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedInteger('upvote');
+            $table->unsignedInteger('downvote');
+
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('pertanyaan_id');
+            $table->unsignedBigInteger('jawaban_id');
+
+            //foreign key ke tabel users
+            $table  ->foreign('user_id')
+                    ->references('id')
+                    ->on('users')
+                    ->onDelete('cascade')
+                    ->onUpdate('cascade');
+
+            //foreign key ke tabel pertanyaan
+            $table  ->foreign('pertanyaan')
+                    ->references('id')
+                    ->on('users')
+                    ->onDelete('cascade')
+                    ->onUpdate('cascade');
+
+             //foreign key ke tabel jawaban
+            $table  ->foreign('jawaban_id')
+                    ->references('id')
+                    ->on('users')
+                    ->onDelete('cascade')
+                    ->onUpdate('cascade');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vote');
+    }
+}
