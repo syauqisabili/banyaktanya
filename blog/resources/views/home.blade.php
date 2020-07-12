f802237ed15ef7725eb40e4f3a5e14addd3d018c
Vito Atmo <bhadrika25@gmail.com>
2020-07-12 16:08:03 +0700

    clean
    revisi minor text

--- blog/resources/views/home.blade.php
+++ blog/resources/views/home.blade.php
@@ -4,38 +4,41 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="Container mr-2 ml-2">
-  <div class="card">
-    <div class="card-body">
-      <h5 class="card-title">Apa yang sedang kamu bingungkan?</h5>
-      <p class="card-text">Jangan ragu untuk mulai bertanya</p>
-      <a href="{{ route('pertanyaan.create') }}" class="btn btn-primary">Mulai Bertanya</a>
+<div class="container">
+    <div class="row">
+        <div class="col-6 col-sm-4">
+
+        </div>
+        <div class="col-6 col-sm-4">
+          <div class="card" style="width: auto;" >
+            <div class="card-body">
+              <h5 class="card-title">Apa yang sedang kamu bingungkan?</h5>
+              <p class="card-text">Jangan ragu untuk mulai bertanya</p>
+              <a href="{{ route('pertanyaan.create') }}" class="btn btn-primary">Mulai Bertanya</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-6 col-sm-4">
+
+        </div>
     </div>
-  </div>
+</div>
+
+
 
 
   <!-- Nanti list banyak pertanyaan-->
-  <div class="row">
 
-  </div>
-  <div class="card">
-  <h5 class="card-header">Featured</h5>
-  <div class="card-body">
-    <h5 class="card-title">Special title treatment</h5>
-    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
 
-<a class="dropdown-item" href="{{ route('logout') }}"
-  onclick="event.preventDefault();
-                document.getElementById('logout-form').submit();">
-  {{ __('Logout') }}
-</a>
+  <a class="dropdown-item" href="{{ route('logout') }}"
+    onclick="event.preventDefault();
+                  document.getElementById('logout-form').submit();">
+    {{ __('Logout') }}
+  </a>
 
-<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-  @csrf
-</form>
+  <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+    @csrf
+  </form>
 
 @endsection
 
